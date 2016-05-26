14ca203c24dd5483aee96b415365426be99c38c4
OCTAU alexandre <alexandre.octau@gmail.com>
2016-05-27 00:37:58 +0200

    [chrome] send platform data to script

--- extensions/chrome/handler.js
+++ extensions/chrome/handler.js
@@ -2,26 +2,29 @@
 	CONTENT SCRIPT HANDLER 
 */
 var manifest = chrome.runtime.getManifest();
-
-//Send extension description to webpage
-window.postMessage({
+var config   = {
 	__WEBUG_AVAILABLE: true,
 	__WEBUG_REVISION : manifest.version,
 	__WEBUG_PLATFORM : this.window.navigator.userAgent || undefined
-}, "*");
+};
+
+//Send extension configuration to webpage
+window.postMessage(config, "*");
 
 //Catch snap event send through the webpage by the webug script
 document.addEventListener("snap", function(data) {
 	//Request screenshot from the main extension
 	chrome.runtime.sendMessage(data, function(picture) {
 		//Dispatch message to the webpage
-		window.postMessage({snap: picture}, "*");
+		window.postMessage({
+			snap:      picture,
+			platform:  config.__WEBUG_PLATFORM
+		}, "*");
 	});
 });
 
 //Catch event send through popup
 chrome.runtime.onMessage.addListener(function (msg, sender, response) {
-	console.log("requested action from popup", msg);
 	if (msg.from === 'popup') {
 		switch (msg.action) {
 			case "clipboard":

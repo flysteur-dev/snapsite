4b59fea74bec51df872891cecc6d001590d77a52
OCTAU alexandre <alexandre.octau@gmail.com>
2016-06-13 21:25:14 +0200

    [lib] add callback when extension is not installed

--- snapsite.js
+++ snapsite.js
@@ -7,7 +7,7 @@ VERSION 0.1
 
 "use strict";
 
-var SnapSite = function (callback, options) {
+var SnapSite = function (callback, options, error) {
 
 	//Library/Extension status
 	var isStarted    = false;
@@ -42,23 +42,9 @@ var SnapSite = function (callback, options) {
 	function start() {
 		//Extension is required
 		//Library won't be launched until browser client install extension
-		if (options.withExtension === true && !hasExtension) {
-			return;
-		}
+		if (options.withExtension === true && !hasExtension) return;
 
 		if (isStarted === false) {
-			//Add trigger
-			var trigger = document.createElement('div');
-			trigger.setAttribute("id", options.selector);
-
-			var  button = document.createElement("img");
-			button.src  = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAQAAABLCVATAAABJklEQVR4Ae3VPYrbUBRA4a+RJ1VSJswvGG8mhYsswRhtxT/YmNEuhtEiAmpcJjBJOQsYyyBMsF0qXcjDjNF7pAnJOd0tTqN3r/yLfPCsFfrsvUje+ao91RdvRfBGpX3Fzy6c4UKh0Ua6U+gJKLSJFgKa5FAjoE23W6hRWpp4UKeHNnIZgMzIS0rom1shXHqKDW3cgoHC2tq9PriyiQvl4JPjr8nBEIxjQo0MA8dguneHTN09VL7ySBfgsXtoCdYnoQpMu4cmZ0Oz7qEHcH8SmoOye6iWoe8QTH+4QWYb8/lHYGj/W+YjyOPe0YtLcGehUpm7Adfq2BV5coWQa9/TlnYcLG2uTj8jtUdTM6VtxBlJNGCXnGn+1PFfCegl/Y4aKz1/Lf/5CShXgVKz4A8DAAAAAElFTkSuQmCC";
-			button.style.cssText = "position:absolute;bottom:0;right:0;width:30px;height:30px;z-index:999;background:#DEDEDE;border-left:1px solid grey;border-top:1px solid grey";
-			trigger.appendChild(button);
-
-			//Insert to the webpage
-			document.body.appendChild(trigger);
-
 			//Ready to catch event
 			var catcher = document.getElementById(options.selector);
 				catcher.addEventListener('click', function() {
@@ -70,18 +56,13 @@ var SnapSite = function (callback, options) {
 					document.dispatchEvent(event);
 				} else {
 					//Extension is not ready, install extension first.
-					fallback();
+					error({"no-extension":"Please install extension first."});
 				}
 			}, false);
 
 			//Library is started
 			isStarted = true;
 		}
-		
-		function fallback() {
-			console.warn("fallback, install extension first!");
-		}
 	}
-	
 	start();
 }
